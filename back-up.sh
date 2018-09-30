7748c98b3b0639f2bf662d8bd4892a74684942fe
Juan Sebastian Hoyos Ayala <juan.hoyos@microsoft.com>
2018-09-29 19:32:01 -0700

    Add simple VIM configuration backup script for Linux

---
+++ back-up.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Backup vimrc
+cp ~/.vimrc .
+
+# Backup extensions
+EXTENSION_DIR_LIST=`ls -1 -d ~/.vim/bundle/*`
+FILE_LIST_REPO="extension_list"
+[[ -f $FILE_LIST_REPO ]] && rm $FILE_LIST_REPO
+
+for EXTENSION_DIR in $EXTENSION_DIR_LIST; do
+    pushd $EXTENSION_DIR > /dev/null
+    REPO_LINK=`git remote get-url origin`
+    popd > /dev/null
+    echo git clone $REPO_LINK >> $FILE_LIST_REPO
+done
+
